d5f7485c4d034d6c53e30e35d4c7599f0b2ff827
Shrey Tiwari <shrey2697@gmail.com>
2020-08-29 16:40:00 +0530

    first commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueTyperPlugin from "vue-typer";
+import ElementUI from "element-ui";
+// import "element-ui/lib/theme-chalk/index.css";
+import "./assets/scss/element-variables.scss";
+import "./assets/scss/global.css";
 
 Vue.config.productionTip = false;
+Vue.use(VueTyperPlugin);
+Vue.use(ElementUI);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
---
+++ src/views/Login/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="login">
+    <div class="left-panel">
+      <div class="heading">SURWAY</div>
+      <div>
+        <h1>
+          Get your surveys done,
+          <vue-typer
+            erase-style="backspace"
+            erase-delay="70"
+            :text="['Easily', 'Quickly', 'Efficiently']"
+          ></vue-typer>
+        </h1>
+      </div>
+    </div>
+    <div class="right-panel">
+      <div class="logo-container">
+        <img src="@/assets/images/Logo.svg" class="logo-design" alt="" />
+      </div>
+      <div class="login-container">
+        Login to create poll
+        <div class="form-container">
+          <el-form>
+            <el-form-item label="Email Address">
+              <el-input placeholder="someone@xyz.com"></el-input>
+            </el-form-item>
+            <el-form-item label="Password">
+              <el-input placeholder="minimum 8 characters"></el-input>
+            </el-form-item>
+            <el-button
+              type="primary"
+              :style="{ marginTop: '4%', width: '100%' }"
+              >Login</el-button
+            >
+          </el-form>
+          <el-divider content-position="center">OR</el-divider>
+          <el-button type="info" :style="{ width: '100%' }"
+            >Respond to a poll</el-button
+          >
+        </div>
+      </div>
+
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Login",
+};
+</script>
+
+<style lang="scss">
+@import "./style.scss";
+</style>
